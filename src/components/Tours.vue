986322f6cd3c4916689629fe8afb01697ad72426
marianamrn <mariana.mrnch@gmail.com>
2024-06-10 22:43:13 +0300

    Connect to database

--- src/components/Tours.vue
+++ src/components/Tours.vue
@@ -1,12 +1,34 @@
 <template>
-    <div>
-      <h1>Tours</h1>
-      <p>Our tours.</p>
+  <div>
+    <h1>Tours</h1>
+    <div v-for="tour in tours" :key="tour.id">
+      <h2>{{ tour.name }}</h2>
+      <img :src="tour.image_path" :alt="tour.name" />
     </div>
-  </template>
-  
-  <script>
-  export default {
-  };
-  </script>
-  
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      tours: []
+    };
+  },
+  created() {
+    this.fetchTours();
+  },
+  methods: {
+    async fetchTours() {
+      try {
+        const response = await axios.get('http://localhost/api/tours.php');
+        this.tours = response.data;
+      } catch (error) {
+        console.error('Error fetching tours:', error);
+      }
+    }
+  }
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue';
 import App from './App.vue';
-import router from './router';
-import './styles/reset.css';  // Імпорт файлу скидання стилів
+import router from './router'; // Імпортування маршрутизатора
+import './styles/reset.css'; // Ваш файл зі стилями
 
 createApp(App)
   .use(router)
